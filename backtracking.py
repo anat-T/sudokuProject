2d06cde6c4ab737bcd91c0bbc3828160a3762025
Anat <anat19960@gmail.com>
2022-01-03 15:27:59 +0200

    feat: complete backtracking

--- backtracking.py
+++ backtracking.py
@@ -14,28 +14,35 @@ def isValidCell(i, j, n):
             return False
 
     # checking square
-    for t in range(i-1, i+2):
-        for w in range(j-1, j+2):
-            if board[t][w] == n:
+    rowStart = (i//3)*3
+    colStart = (j//3)*3
+    for t in range(0, 3):
+        for w in range(0, 3):
+            if board[rowStart+t][colStart+w] == n:
                 return False
 
     return True
 
 
-# print(isValidCell(1, 1, 9))
+def isBoardComplete(board):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            if board[i][j] == 0:
+                return False
+    return True
 
 
 def backtrackingSol(board):
     # global board
-    oldBoard = board
-    for i in range(0, 9):
-        for j in range(0, 9):
-            if board[i][j] == 0:
-                for n in range(1, 10):
-                    if isValidCell(i, j, n):
-                        board[i][j] = n
-                        backtrackingSol(board)
-                        board[i][j] = 0
-                return
-    if oldBoard == board:
-        return board
+    while (not isBoardComplete(board)):
+        for i in range(0, 9):
+            for j in range(0, 9):
+                if board[i][j] == 0:
+                    for n in range(1, 10):
+                        if isValidCell(i, j, n):
+                            board[i][j] = n
+                            oldBoard = board
+                            backtrackingSol(board)
+                            if oldBoard == board:
+                                return board
+                            board[i][j] = 0
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from backtracking import *
 from crooks import *
+import time
 
 
 def main():
-    print(backtrackingSol(board))
+    start_time = time.time()
+    solution = backtrackingSol(board)
+    print(solution)
+    print("--- %s seconds ---" % (time.time() - start_time))
 #     possible_value = [[]]
 #     check_row(possible_value, board)
 
